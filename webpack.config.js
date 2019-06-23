7c7c873fca9d68e21b9861d6136e4f8fbe521f3d
naotaro0123 <n.ikewada@gmail.com>
2019-06-23 15:57:40 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.ts',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.ts$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './src'),
+    watchContentBase: true,
+    port: 3000,
+    open: true,
+    openPage: './index.html'
+  }
+};
